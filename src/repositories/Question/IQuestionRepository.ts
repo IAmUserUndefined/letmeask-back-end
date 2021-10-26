4850ff4428810f4cb4451a68111c134e986a1f28
João Pedro <jpmns9103@gmail.com>
2021-10-26 00:51:09 -0300

    criação da classe, da interface e dos testes do repositório de questão

---
+++ src/repositories/Question/IQuestionRepository.ts
@@ -0,0 +1,9 @@
+import { Question } from "../../entities/Question";
+
+interface IQuestionRepository {
+    store(id: string, roomId: string, name: string): Promise<void>;
+    getQuestions(roomId: string): Promise<Question[]>;
+    destroy(roomId: string): Promise<void>;
+}
+
+export default IQuestionRepository;
---
+++ src/repositories/Question/QuestionRepository.ts
@@ -0,0 +1,34 @@
+import IQuestionRepository from "./IQuestionRepository";
+import QuestionModel from "../../database/models/Question";
+import { Question } from "../../entities/Question";
+import { EntityRepository, getCustomRepository } from "typeorm";
+
+@EntityRepository(Question)
+export class QuestionRepository implements IQuestionRepository {
+	
+	private repository: QuestionModel;
+
+	constructor(){
+		this.repository = getCustomRepository(QuestionModel);
+	}
+
+	async store(id: string, roomId: string, name: string): Promise<void> {
+		const room = this.repository.create({
+			id: id,
+			roomId: roomId,
+			name: name
+		});
+
+		await this.repository.save(room);
+	}
+
+	async getQuestions(): Promise<Question[]> {
+		const questions = await this.repository.find();
+		return questions;
+	}
+
+	async destroy(id: string): Promise<void> {
+		await this.repository.delete(id);
+	}
+
+}
