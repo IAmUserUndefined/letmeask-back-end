36c514c0f85980995e94f44e97369a70b3c5e1c0
João Pedro <jpmns9103@gmail.com>
2021-11-11 00:44:08 -0300

    recriar repositório das questões e seus testes

---
+++ src/repositories/Question/QuestionRepository/QuestionRepository.ts
@@ -0,0 +1,33 @@
+import prisma from "../../../../prisma/index";
+import { Question } from "../../../entities/Question";
+import IQuestionRepository from "./IQuestionRepository";
+
+export class QuestionRepository implements IQuestionRepository {
+	
+	async store(id: string, roomId: string, name: string): Promise<void> {
+		await prisma.question.create({
+			data: {
+				id: id,
+				roomId: roomId,
+				name: name
+			}
+		});
+	}
+
+	async getQuestions(roomId: string): Promise<Question[]> {
+		const questions = await prisma.question.findMany({
+			where: {
+				roomId: roomId
+			}
+		});
+		return questions;
+	}
+
+	async destroy(id: string): Promise<void> {
+		await prisma.question.delete({
+			where: {
+				id: id
+			}
+		});
+	}
+}
