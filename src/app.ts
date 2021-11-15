815a5e5dbde366b36480641f5cf19dc5bf7301d0
João Pedro <jpmns9103@gmail.com>
2021-11-14 21:51:12 -0300

    adicionar cors a aplicação e adicionar aplicação ao servidor

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "./middlewares/cors";
 import router from "./router/index";
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(router);
 
